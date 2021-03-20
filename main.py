bf28a150b5000d726107c39bcf1dc3e5d94d1098
alexeisei <80957733+alexeisei@users.noreply.github.com>
2021-03-20 18:39:49 +0100

    Update main.py
    Avancée sur la recherche des divers éléments pour 1 livre
    via recherche dans l'arbre, dans liste...

--- main.py
+++ main.py
@@ -11,17 +11,29 @@ response = requests.get(url)
  #si la requête = ok (200), création de "soup" et recherche de title dedans, puis afficher le résultat en .txt
 if response.ok:
     soup = BeautifulSoup(response.text, features="html.parser")
+
+
+    upc = soup.tr.td.text
+    #upc2 = soup.th.next_sibling.text
     title = soup.find('h1')
-    tom = soup.findAll('td')
+    infos = soup.findAll('td')
+    price_excluding_tax = infos[2].text
+    price_including_tax = infos[3].text
+    number_available = infos[5].text
+    # product_description = soup.find(id="content_inner")
+    cat = soup.ul.findAll("li")
+    category = cat[2].text.strip()
+    review_rating = soup.find("p", class_="star-rating")["class"][1]
+    image_url = soup.img["src"].replace("..", "")
 
 
-#universal_product_code = soup.find("upc")
-#product_page_url = soup.find
 
-
+    print(upc)
     print(title.text)
-    print(tom)
-  
- #création de branche
-
-
+    print(price_excluding_tax)
+    print(price_including_tax)
+    print(number_available)
+    #print(product_description)
+    print(poetry)
+    print(review_rating)
+    print(image_url)
