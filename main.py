1ae2432e59d7af99b0e3927ee8ebd3555b75f407
alexeisei <80957733+alexeisei@users.noreply.github.com>
2021-04-06 23:07:45 +0200

    Clarification en supprimant des variables

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ if response.ok:
     soup = BeautifulSoup(response.content, features='html.parser')
     containers = soup.find(class_='nav nav-list').find_all('a')[1:]
     for container in containers:
-        link_to_extract = container['href']
-        cat_link = 'https://books.toscrape.com/' + link_to_extract
-        list_categories.append(cat_link)
+        list_categories.append('https://books.toscrape.com/' + container['href'])
 
-# extraction des urls pour les livres d'1 catégorie sur première page
+# extraction des urls pour les livres d'1 catégorie sur la première page
 all_books = []
 for url in list_categories:
     response = requests.get(url)
@@ -46,8 +44,8 @@ for url in list_categories:
                 all_books.append(pages)
                 next_page = soup.find('li', class_='next')
 
-# extraction des éléments de chacun des livres de la catégorie et alimentation des listes afin de créer
-# la base de données pour export csv
+# extraction des éléments de chacun des livres de la catégorie et alimentation du dictionnaire afin de créer la base
+# de données pour export csv
 product_page_urls = []
 upcs = []
 titles = []
@@ -63,48 +61,30 @@ for url in all_books:
     response = requests.get(url)
     if response.ok:
         soup = BeautifulSoup(response.content, features='html.parser')
-        product_page_url = str(url)
-        product_page_urls.append(product_page_url)
-        upc = soup.tr.td.text
-        upcs.append(upc)
-        title = soup.find('h1').text
-        titles.append(title)
+        product_page_urls.append(url)
+        upcs.append(soup.tr.td.text)
+        titles.append(soup.find('h1').text)
         infos = soup.findAll('td')
-        price_excluding_tax = infos[2].text
-        prices_excluding_tax.append(price_excluding_tax)
-        price_including_tax = infos[3].text
-        prices_including_tax.append(price_including_tax)
-        number_available = infos[5].text
-        numbers_available.append(number_available)
-        product_description = soup.find_all('p')
-        product_description = product_description[3].text
-        product_descriptions.append(product_description)
+        prices_excluding_tax.append(infos[2].text)
+        prices_including_tax.append(infos[3].text)
+        numbers_available.append(infos[5].text)
+        product_descriptions.append(soup.find_all('p')[3].text)
         category = soup.ul.find_all('li')
         category = category[2].text.strip()
         categories.append(category)
-        review_rating = soup.find('p', class_='star-rating')['class'][1]
-        reviews_rating.append(review_rating)
-        image_url = 'https://books.toscrape.com/' + soup.img['src'].replace('..', (''))
-        images.append(image_url)
+        reviews_rating.append(soup.find('p', class_='star-rating')['class'][1])
+        images.append('https://books.toscrape.com/' + soup.img['src'].replace('..', ''))
 
-        data_to_extract = {
-            'Category': categories,
-            'Universal Product Code': upcs,
-            'Title': titles,
-            'Product Page Url': product_page_urls,
-            'Product Description': product_descriptions,
-            'Price Excluding Tax': prices_excluding_tax,
-            'Price Including Tax': prices_including_tax,
-            'Review Rating': reviews_rating,
-            'Number Available': numbers_available,
-            'Image Url': images
-        }
+data_to_extract = {'Category': categories, 'Universal Product Code': upcs, 'Title': titles,
+                   'Product Page Url': product_page_urls, 'Product Description': product_descriptions,
+                   'Price Excluding Tax': prices_excluding_tax, 'Price Including Tax': prices_including_tax,
+                   'Review Rating': reviews_rating, 'Number Available': numbers_available, 'Image Url': images}
 
-# création de la DataFrame, et export des données splittées par catégorie
+# création de la DataFrame et export des données splittées par catégorie
 os.mkdir('./Données extraites/')
 path = './Données extraites/'
 df = pandas.DataFrame(data=data_to_extract)
-df_by_cat = df.groupby("Category")
+df_by_cat = df.groupby('Category')
 for (category, category_df) in df_by_cat:
     filename = category + ".csv"
     category_df.to_csv(path + filename, index=False,  sep=';', encoding='utf-8-sig')
